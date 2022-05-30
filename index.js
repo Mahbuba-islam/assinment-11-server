c31f152a4ee31dadd4c209818afb260527905a80
mahbuba <mahbubaislam7010@gmail.com>
2022-05-30 11:57:28 -0400

    done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 require('dotenv').config();
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 const app = express();
 
 //midleware
@@ -20,54 +22,100 @@ async function run(){
     try{
       await client.connect();
       const inventoryCollection = client.db('wareHouseManagement').collection('inventoryItem');
-      const myItemsIinventoryCollection = client.db('wareHouseManagement').collection('myItems');
+      const myItemsCollection = client.db('wareHouseManagement').collection('myItems');
      
       const managementCollection = client.db('wareHouseManagement').collection('manageMent');
-     
 
+      //login auth
+      app.post('/login', async (req, res) => {
+        const user = req.body;
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: '1d'
+        });
+        res.send({ accessToken });
+    })
+     
+        //get inventory product
       app.get('/inventoryItem', async (req, res) => {
         const query = {};
         const cursor = inventoryCollection.find(query).limit(6);
         const inventories = await cursor.toArray();
         res.send(inventories);
       })
-
+            //get inventory product details by id
       app.get('/inventoryItem/:id', async(req , res) =>{
         const id = req.params.id;
         const query = {_id: ObjectId(id)};
         const inventoryItem = await inventoryCollection.findOne(query);
         res.send(inventoryItem);
       })
-
+           //get inventory all product
       app.get('/inventoryItems', async (req, res) => {
         const query = {};
         const cursor = inventoryCollection.find(query);
         const inventories = await cursor.toArray();
         res.send(inventories);
       })
-      
+       
+           //get management data
       app.get('/manageMent', async (req, res) => {
         const query = {};
         const cursor = managementCollection.find(query);
         const managementQuit = await cursor.toArray();
         res.send(managementQuit);
       })
-      
-      app.get('/myItems', async (req, res) => {
-        const query = {};
-        const cursor = myItemsIinventoryCollection.find(query);
-        const myItemsInventory = await cursor.toArray();
-        res.send(myItemsInventory);
+
+      //new myItems load api
+      app.post("/uploadItem", async (req,res) =>{
+        const product = req.body;
+        const tokenInfo = req.headers.authorization;
+        const [email, accessToken] = tokenInfo.split(' ')
+
+        const decoded = verifyToken(accessToken)
+
+        if(email === decoded.email){
+          const result = await myItemsCollection .insertOne(product);
+          res.send({success: 'product upload successfully', result})
+        }
+        else{
+          res.send ({success: 'unthorized access'}) 
+        }
       })
 
+      
+      app.get("/addItem", async(req,res) => {
+          const listInfo = req.body;
+          const result = await myItemsCollection.insertOne(listInfo);
+          res.send({success: 'items added complete', result})
+        })
+
+
+        app.get("/uploadList", async (req,res) =>{
+          const tokenInfo = req.headers.authorization;
+          const [email, accessToken] = tokenInfo.split(' ')
+          const decoded = verifyToken(accessToken)
+         if(email === decoded.email){
+            const items = await myItemsCollection.find({email:email}).toArray();
+            res.send(items)
+          }
+          else{
+            res.send ({success: 'unthorized access'}) 
+          }
+        })
+        
+
+      
+          
+      
      
-      // POST
-      app.post('/myItems', async(req, res) =>{
-        const myNewInventory = req.body;
-        const result = await myItemsIinventoryCollection.insertOne(myNewInventory );
-        res.send(result);
-    });
-      // POST
+        // POST for add myItems
+      //   app.post('/myItems', async (req, res) => {
+      //     const myItem = req.body;
+      //     const result = await myItemsCollection.insertOne(myItem);
+      //     res.send(result);
+      // })
+
+        // POST for add new inventory item
       app.post('/inventoryItems', async(req, res) =>{
         const newInventory = req.body;
         const result = await inventoryCollection.insertOne(newInventory);
@@ -75,35 +123,32 @@ async function run(){
     });
 
    
-          //quantity added
-    app.put('/inventoryItem/:id', async(req, res) =>{
-      const id = req.params.id;
-      const inventory = req.body
-      console.log('from update api',inventory)
-      const filter = {_id : ObjectId(id)}
-      const options = {upsert:true};
-      
-      const updateDoc = {
-        $set: 
-          {quantity: updateQuantity},
-        };
+          //quantity reStock
+          app.put('/inventoryItem/:id', async(req, res) =>{
+            // const id = req.params.id;
+            const newProduct = req.body
+            // const filter = {_id : ObjectId(id)}
+          //   const options = {upsert:true};
           
-       
-      
-      const result = await inventoryCollection.updateOne(filter , updateDoc, options);
-      res.send(result);
-  });
+          //   const updateDoc = {
+          //     $set: newProduct
+          //   };
+            
+            
+           const result = await inventoryCollection.insertOne(newProduct);
+            res.send(result);
+        });
+
 
         //quantity decrease
       app.put('/inventoryItem/:id', async(req, res) =>{
         const id = req.params.id;
-        const inventory = req.body
-        console.log('from update api',inventory)
-        const filter = {_id : ObjectId(id)}
+        const updatedProduct = req.body
+       const filter = {_id : ObjectId(id)}
         const options = {upsert:true};
       
         const updateDoc = {
-          $set: {quantity: updatedQuantity},
+          $set: updatedProduct
         };
         
         
@@ -146,3 +191,19 @@ app.get('/', (req, res) => {
 app.listen(port , () =>{
     console.log('listening to port')
 })
+
+  //  verify token
+function verifyToken(token){
+  let email;
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+    if(err){
+      email='invalid email'
+    }
+    if(decoded){
+      console.log(decoded)
+      email = decoded
+    }
+  })
+  return email;
+  };
+
